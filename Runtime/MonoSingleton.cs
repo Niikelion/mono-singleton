4db357f93da8cbf1622a609285374c558dd40236
Niikelion <nicraft@tlen.pl>
2023-12-02 22:13:31 +0100

    init

---
+++ Runtime/MonoSingleton.cs
@@ -0,0 +1,43 @@
+﻿using JetBrains.Annotations;
+using UnityEngine;
+
+namespace Singletons
+{
+    [PublicAPI]
+    public class MonoSingleton<T> where T: MonoBehaviour
+    {
+        private T thisT;
+        
+        /// <summary>
+        /// Single instance of type <typeparamref name="T"></typeparamref>
+        /// <remarks>Can be null when no object is available</remarks>
+        /// </summary>
+        [CanBeNull] public static T Instance { get; private set; }
+
+        /// <summary>
+        /// Method called from duplicate object when it is registering
+        /// </summary>
+        /// <param name="previous">previous instance</param>
+        /// <returns><b>false</b> if duplicate should delete itself, <b>true</b> otherwise</returns>
+        protected virtual bool OnDuplicate([CanBeNull] T previous) => false;
+        
+        /// <summary>
+        /// Registers instance
+        /// <remarks>Remember to call it from inheriting class when overriding <b>Awake</b></remarks>
+        /// </summary>
+        protected virtual void Awake()
+        {
+            thisT = this as T;
+            
+            if (Instance != null)
+            {
+                if (!OnDuplicate(Instance))
+                    Object.Destroy(thisT);
+                
+                return;
+            }
+
+            Instance = thisT;
+        }
+    }
+}
---
+++ Runtime/Singleton.cs
@@ -0,0 +1,19 @@
+﻿using JetBrains.Annotations;
+
+namespace Singletons
+{
+    /// <summary>
+    /// Base class for singletons
+    /// </summary>
+    /// <typeparam name="T">Type inheriting from <see cref="Singleton{T}"/></typeparam>
+    [PublicAPI]
+    public class Singleton<T> where T: new()
+    {
+        /// <summary>
+        /// Lazily instantiated single instance of type <typeparamref name="T"/>
+        /// </summary>
+        [NotNull] public static T Instance => instance ??= new T();
+        
+        private static T instance;
+    }
+}
